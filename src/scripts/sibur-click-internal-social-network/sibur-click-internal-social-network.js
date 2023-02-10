e3c77fec58efca7a7ad8ed940d949a3f0177f4d6
Ivan Ivashin <ivashin@acaudio.com>
2023-02-10 15:17:19 +0700

    feat: sibur, simons

---
+++ src/scripts/sibur-click-internal-social-network/sibur-click-internal-social-network.js
@@ -0,0 +1,283 @@
+import '../../styles/styles.css';
+
+document.getElementById(
+  'sibur-click-internal-social-network'
+).innerHTML = /*html*/ `
+    <div class="sibur-click-internal-social-network-page-container">
+        <div class="header sibur-click-internal-social-network-header">
+            <div class="page-wrapper">
+                <div>
+                    <img src="http://192.168.1.112:8080/wp-content/uploads/2023/02/Sibur_click_Logo-1.png" alt="logo" class="logo" />
+                </div>
+                <h1 class="title sibur-click-internal-social-network-title">Интеграция транспондера<br>в мобильное приложение</h1>
+                <div class="params">
+                    <p class="param direction sibur-click-internal-social-network-direction">Мобильное решение</p>
+                    <p class="param competence sibur-click-internal-social-network-competence">Ритейл</p>
+                </div>
+                <div class="device-types-list">
+                    <div class="device-type-container">
+                        <div class="device-types-list-marker sibur-click-internal-social-network-device-types-list-marker"></div>
+                        <p class="device-type sibur-click-internal-social-network-device-type">Android</p>
+                    </div>
+                    <div class="device-type-container">
+                        <div class="device-types-list-marker sibur-click-internal-social-network-device-types-list-marker"></div>
+                        <p class="device-type sibur-click-internal-social-network-device-type">IOS</p>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <img src="http://192.168.1.112:8080/wp-content/uploads/2023/02/sibur-click-internal-social-network-header-illustration.png" alt="header-illustration" class="sibur-click-internal-social-network-header-illustration" />
+            </div>
+        </div>
+
+        <div class="page-wrapper">
+            <div class="content-wrapper">
+                <p class="paragraph">В период пандемии коронавируса СИБУР осознал необходимость нового элемента в системе цифровых коммуникаций между сотрудниками - корпоративной деловой сети.</p>
+                <p class="paragraph paragraph-margin-top">Новый продукт должен был прийти на смену корпоративному порталу, и стать универсальным цифровым рабочим пространством и внести вклад в повышение качества взаимодействия с внешними партнерами, за счет развития коммуникационных процессов внутри СИБУРа с опорой на ценности корпоративной культуры компании. </p>
+            </div>
+
+            <section class="section">
+                <h2 class="section-title">Задача</h2>
+                <p class="paragraph content-wrapper">Создать пространство для удобного и эффективного обмена корпоративной информацией, совместного ведения проектов, публикации новостей.</p>
+            </section>
+
+            <section class="section">
+                <h2 class="section-title">Решение</h2>
+                <div class="block-with-definition">
+                    <div class="text-area content-wrapper">
+                        <p class="paragraph">Проект для нас был интересным со всех сторон – с позиции проектирования интерфейсов, реализации бизнес-функций и технологического стека.</p>
+                        <p class="paragraph paragraph-margin-top">Экранные формы создавались с учетом онбординга – удобного погружения пользователя в продукт.</p>
+                        <p class="paragraph paragraph-margin-top">Продукт выступает как проактивный помощник, который заботится о том, чтобы сделать работу с приложением легкой и удобной. Масштаб функциональности впечатляет.</p>
+                        <p class="paragraph paragraph-margin-top">Приложение создано для целей корпорации, одновременно с этим, набор возможностей делает продукт конкурентоспособным на открытом рынке.</p>
+                        <p class="paragraph paragraph-margin-top">Мобильный клиент для IOS и Android предлагает следующий функционал:</p>
+                    </div>
+                    <div class="definition-area">
+                        <div class="definition-container sibur-click-internal-social-network-definition-container">
+                            <h3 class="sibur-click-internal-social-network-definition sibur-click-internal-social-network-definition-color z-index">ОНБОРДИНГ</h3>
+                            <p class="paragraph paragraph-margin-top sibur-click-internal-social-network-definition-color z-index">
+                                процесс адаптация<br>клиента к новому<br>продукту
+                            </p>
+                            <div>
+                                <img src="http://192.168.1.112:8080/wp-content/uploads/2023/02/sibur-click-internal-social-network-definition-illustration.png" alt="definition-illustration" class="sibur-click-internal-social-network-definition-illustration" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <ul class="content-wrapper paragraph-margin-top sibur-click-internal-social-network-functionality-list">
+                    <li class="simple-list-item sibur-click-internal-social-network-functionality-list-item">
+                        <div class="sibur-click-internal-social-network-functionality-list-marker marker-008C95"></div>
+                        <p class="paragraph">Авторизация через Air Watch</p>
+                    </li>
+                    <li class="simple-list-item sibur-click-internal-social-network-functionality-list-item">
+                        <div class="sibur-click-internal-social-network-functionality-list-marker marker-008C95"></div>
+                        <p class="paragraph">Профили пользователей</p>
+                    </li>
+                    <li class="simple-list-item sibur-click-internal-social-network-functionality-list-item">
+                        <div class="sibur-click-internal-social-network-functionality-list-marker marker-EC6400"></div>
+                        <p class="paragraph">Чат</p>
+                    </li>
+                    <li class="simple-list-item sibur-click-internal-social-network-functionality-list-item">
+                        <div class="sibur-click-internal-social-network-functionality-list-marker marker-EC6400"></div>
+                        <p class="paragraph">Уведомления о событиях</p>
+                    </li>
+                    <li class="simple-list-item sibur-click-internal-social-network-functionality-list-item">
+                        <div class="sibur-click-internal-social-network-functionality-list-marker marker-FFA73F"></div>
+                        <p class="paragraph">Обмен видео файлами и документами</p>
+                    </li>
+                    <li class="simple-list-item sibur-click-internal-social-network-functionality-list-item">
+                        <div class="sibur-click-internal-social-network-functionality-list-marker marker-FFA73F"></div>
+                        <p class="paragraph">Менеджер загрузки файлов</p>
+                    </li>
+                    <li class="simple-list-item sibur-click-internal-social-network-functionality-list-item">
+                        <div class="sibur-click-internal-social-network-functionality-list-marker marker-4FC6CE"></div>
+                        <p class="paragraph">Лента новостей, подписка на новости пользователей</p>
+                    </li>
+                    <li class="simple-list-item sibur-click-internal-social-network-functionality-list-item">
+                        <div class="sibur-click-internal-social-network-functionality-list-marker marker-4FC6CE"></div>
+                        <p class="paragraph">Управление группами. Публичные и приватные группы</p>
+                    </li>
+                    <li class="simple-list-item sibur-click-internal-social-network-functionality-list-item">
+                        <div class="sibur-click-internal-social-network-functionality-list-marker marker-008C95"></div>
+                        <p class="paragraph">Репосты</p>
+                    </li>
+                    <li class="simple-list-item sibur-click-internal-social-network-functionality-list-item">
+                        <div class="sibur-click-internal-social-network-functionality-list-marker marker-008C95"></div>
+                        <p class="paragraph">Менеджер видео и фото</p>
+                    </li>
+                    <li class="simple-list-item sibur-click-internal-social-network-functionality-list-item">
+                        <div class="sibur-click-internal-social-network-functionality-list-marker marker-EC6400"></div>
+                        <p class="paragraph">Поиск по новостям, пользователям, группам</p>
+                    </li>
+                    <li class="simple-list-item sibur-click-internal-social-network-functionality-list-item">
+                        <div class="sibur-click-internal-social-network-functionality-list-marker marker-EC6400"></div>
+                        <p class="paragraph">Многоязычный интерфейс приложения</p>
+                    </li>
+                    <li class="simple-list-item sibur-click-internal-social-network-functionality-list-item">
+                        <div class="sibur-click-internal-social-network-functionality-list-marker marker-FFA73F"></div>
+                        <p class="paragraph">Обратная связь со службой поддержки</p>
+                    </li>
+                </ul>
+                <div class="sibur-click-internal-social-network-decision-illustration-container">
+                    <div>
+                        <img src="http://192.168.1.112:8080/wp-content/uploads/2023/02/sibur-click-internal-social-network-decision-illustration-1.png" alt="decision-illustration-1" class="sibur-click-internal-social-network-decision-illustration-1" />
+                    </div>
+                    <div>
+                        <img src="http://192.168.1.112:8080/wp-content/uploads/2023/02/sibur-click-internal-social-network-decision-illustration-2.png" alt="decision-illustration-2" class="sibur-click-internal-social-network-decision-illustration-2" />
+                    </div>
+                    <div>
+                        <img src="http://192.168.1.112:8080/wp-content/uploads/2023/02/sibur-click-internal-social-network-decision-illustration-3.png" alt="decision-illustration-3" class="sibur-click-internal-social-network-decision-illustration-3" />
+                    </div>
+                    <div>
+                        <img src="http://192.168.1.112:8080/wp-content/uploads/2023/02/sibur-click-internal-social-network-decision-illustration-4.png" alt="decision-illustration-4" class="sibur-click-internal-social-network-decision-illustration-4" />
+                    </div>
+                </div>
+            </section>
+
+            <section class="section">
+                <h2 class="section-title">Технологии</h2>
+                <p class="paragraph content-wrapper">При разработке архитектуры приложения заложили возможность для расширения. Приложение разделено на функциональные модули, что позволяет гибко управлять доступным функционалом и расширять функциональность с меньшими изменениями.</p>
+                <ul class="technology-list content-wrapper paragraph-margin-top sibur-click-internal-social-network-technology-list">
+                    <li class="technology-container">
+                        <div>
+                            <img src="http://192.168.1.112:8080/wp-content/uploads/2023/02/Android-Architecture-Components.png" alt="android" class="technology-logo" />
+                        </div>
+                        <div class="technology-name-container">
+                            <p class="technology-name">Android</p>
+                            <p class="technology-sub-name">Architecture Components</p>
+                        </div>
+                    </li>
+                    <li class="technology-container">
+                        <div>
+                            <img src="http://192.168.1.112:8080/wp-content/uploads/2023/02/kotlin.png" alt="kotlin" class="technology-logo" />
+                        </div>
+                        <p class="technology-name">Kotlin</p>
+                    </li>
+                    <li class="technology-container">
+                        <div>
+                            <img src="http://192.168.1.112:8080/wp-content/uploads/2023/02/Kotlin-Coroutines.png" alt="kotlin-coroutines" class="technology-logo" />
+                        </div>
+                        <p class="technology-name">Kotlin<br>Coroutines</p>
+                    </li>
+                    <li class="technology-container">
+                        <div>
+                            <img src="http://192.168.1.112:8080/wp-content/uploads/2023/02/JSON-Kotlinx.-Serialization.png" alt="json-kotlinx-serialization" class="technology-logo" />
+                        </div>
+                        <div class="technology-name-container">
+                            <p class="technology-name">JSON</p>
+                            <p class="technology-sub-name">Kotlinx.<br>Serialization</p>
+                        </div>
+                    </li>
+                    <li class="technology-container">
+                        <div>
+                            <img src="http://192.168.1.112:8080/wp-content/uploads/2023/02/OkHttp.png" alt="ok-http" class="technology-logo" />
+                        </div>
+                        <p class="technology-name">OkHttp</p>
+                    </li>
+                    <li class="technology-container">
+                        <div>
+                            <img src="http://192.168.1.112:8080/wp-content/uploads/2023/02/Retrofit.png" alt="retrofit" class="technology-logo" />
+                        </div>
+                        <p class="technology-name">Retrofit</p>
+                    </li>
+                    <li class="technology-container">
+                        <div>
+                            <img src="http://192.168.1.112:8080/wp-content/uploads/2023/02/Glide.png" alt="glide" class="technology-logo" />
+                        </div>
+                        <p class="technology-name">Glide</p>
+                    </li>
+                    <li class="technology-container">
+                        <div>
+                            <img src="http://192.168.1.112:8080/wp-content/uploads/2023/02/VMware-Workspace-ONE.png" alt="vmware-workspace-one" class="technology-logo" />
+                        </div>
+                        <p class="technology-name">VMware<br>Workspace ONE</p>
+                    </li>
+                    <li class="technology-container">
+                        <div>
+                            <img src="http://192.168.1.112:8080/wp-content/uploads/2023/02/Firebase-Messaging.png" alt=" firebase-messaging" class="technology-logo" />
+                        </div>
+                        <p class="technology-name">Firebase<br>Messaging</p>
+                    </li>
+                    <li class="technology-container">
+                        <div>
+                            <img src="http://192.168.1.112:8080/wp-content/uploads/2023/02/Dagger2.png" alt="dagger2" class="technology-logo" />
+                        </div>
+                        <p class="technology-name">Dagger2</p>
+                    </li>
+                    <li class="technology-container">
+                        <div>
+                            <img src="http://192.168.1.112:8080/wp-content/uploads/2023/02/Lottie.png" alt="lottie" class="technology-logo" />
+                        </div>
+                        <p class="technology-name">Lottie</p>
+                    </li>
+                    <li class="technology-container">
+                        <div>
+                            <img src="http://192.168.1.112:8080/wp-content/uploads/2023/02/JUnit4.png" alt="JUnit4" class="technology-logo" />
+                        </div>
+                        <p class="technology-name">JUnit4</p>
+                    </li>
+                    <li class="technology-container">
+                        <div>
+                            <img src="http://192.168.1.112:8080/wp-content/uploads/2023/02/Espresso.png" alt="Espresso" class="technology-logo" />
+                        </div>
+                        <p class="technology-name">Espresso</p>
+                    </li>
+                    <li class="technology-container">
+                        <div>
+                            <img src="http://192.168.1.112:8080/wp-content/uploads/2023/02/Mockk.png" alt="Mockk" class="technology-logo" />
+                        </div>
+                        <p class="technology-name">Mockk</p>
+                    </li>
+                    <li class="technology-container">
+                        <div>
+                            <img src="http://192.168.1.112:8080/wp-content/uploads/2023/02/Hyperion.png" alt="Hyperion" class="technology-logo" />
+                        </div>
+                        <p class="technology-name">Hyperion</p>
+                    </li>
+                </ul>
+            </section>
+
+            <section>
+                <div class="section staff-comment-section">
+                    <div class="comment-container sibur-click-internal-social-network-comment-container">
+                        <p class="paragraph">
+                            Наша команда получила отличные практические знания по разработке приложения с интеграцией Platform VMware Workspace ONE.
+                        </p>
+                        <p class="paragraph paragraph-margin-top">
+                        Положительные отзывы и идеи от пользователей сети в режиме реального времени, помогали реализовывать все больше полезного функционала в кратчайшие сроки.
+                        </p>
+                        <div class="staff-container">
+                            <p class="staff-name">
+                                Денисов Максим
+                            </p>
+                            <p class="staff-post">
+                                Руководитель отдела мобильных решений
+                            </p>
+                        </div>
+                    </div>
+                    <div>
+                        <img src="http://192.168.1.112:8080/wp-content/uploads/2023/02/DenisMaksimov.png" alt="denis-maksimov" class="staff-photo" />
+                    </div>
+                </div>
+                <div class="section staff-comment-section mirroring">
+                    <div class="comment-container sibur-click-internal-social-network-customer-comment-container">
+                        <p class="paragraph mirroring">
+                            Мобильная социальная сеть позволила быстрее решать возникающие вопросы от сотрудников находящихся на производстве. Руководителям стало проще доносить новую информацию до сотрудников.
+                        </p>
+                    </div>
+                    <div>
+                        <img src="http://192.168.1.112:8080/wp-content/uploads/2023/02/Sibur_otziv.png" alt="sibur-otziv" class="staff-photo mirroring" />
+                    </div>
+                </div>
+            </section>
+        </div>
+
+        <div class="footer">
+            <div class="page-wrapper">
+                <h3 class="footer-title">Похожие кейсы</h3>
+                <div>
+                    <OtherProjectsComponent ids="75" />
+                </div>
+            </div>
+        </div>
+    </div>
+`;
---
+++ src/scripts/simons-voss-transponder/simons-voss-transponder.js
@@ -0,0 +1,136 @@
+import '../../styles/styles.css';
+
+document.getElementById('simons-voss-transponder').innerHTML = /*html*/ `
+    <div class="header simons-voss-transponder-header">
+        <div class="page-wrapper">
+            <div>
+                <img src="http://192.168.1.112:8080/wp-content/uploads/2023/02/SimonsVoss_logo.png" alt="logo" class="logo" />
+            </div>
+            <h1 class="title simons-voss-transponder-header-title">Интеграция транспондера<br>в мобильное приложение</h1>
+            <div class="params">
+                <p class="param direction simons-voss-transponder-direction">Мобильное решение</p>
+                <p class="param competence simons-voss-transponder-competence">Ритейл</p>
+            </div>
+            <div class="device-types-list">
+                <div class="device-type-container">
+                    <div class="device-types-list-marker simons-voss-transponder-device-types-list-marker"></div>
+                    <p class="device-type simons-voss-transponder-device-type">Android</p>
+                </div>
+                <div class="device-type-container">
+                    <div class="device-types-list-marker simons-voss-transponder-device-types-list-marker"></div>
+                    <p class="device-type simons-voss-transponder-device-type">IOS</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="page-wrapper">
+        <div class="content-wrapper">
+            <p class="paragraph">SimonsVoss -немецкий производитель безпроводных замковых систем. С помощью такой системы можно без особого труда установить надежную замочную систему и организовать контроль проходов. И все это без единого провода и в считанные минуты.</p>
+            <p class="paragraph paragraph-margin-top">Теперь Вы можете забыть про связки ключей, которые оттягивают Вам карманы. Один программируемый транспондер заменяет собой все ключи и карты. Нажатие кнопки на транспондера открывает перед Вами двери кабинетов, офисов, лифтов, управляет шлагбаумом, охранной сигнализацией и другими исполнительными устройствами. </p>
+            <p class="paragraph paragraph-margin-top">Это невероятно удобная замочная система, но вам все еще необходимо носить с собой физическое устройство.  Желая сделать свой продукт еще удобнее компания Simons Voss обратилась в Интабию с запросом на разработку мобильного решения полностью заменяющего транспондер.</p>
+        </div>
+
+        <section class="section">
+            <h2 class="section-title">Задача</h2>
+            <p class="paragraph">Интегрировать транспондер в мобильное приложение.</p>
+        </section>
+
+        <section class="section">
+            <h2 class="section-title">Решение</h2>
+            <div class="block-with-definition">
+                <div class="text-area content-wrapper">
+                    <p class="paragraph">Создана библиотека для работы с замками и инфраструктура для управления транспондерами. В мобильное приложение интегрирована библиотека и добавлена возможность создания и управления транспондерами.</p>
+                    <p class="paragraph">Теперь пользователю не нужно носить Транспондер с собой, весь необходимый функционал для работы с замками у него в телефоне.</p>
+                </div>
+                <div class="definition-area">
+                    <div class="definition-container simons-voss-transponder-definition-container">
+                        <h3 class="simons-voss-transponder-definition z-index">ТРАНСПОНДЕР</h3>
+                        <p class="paragraph paragraph-margin-top z-index">
+                            физическое устройство для открытия умных замков
+                        </p>
+                        <div>
+                            <img src="http://192.168.1.112:8080/wp-content/uploads/2023/02/simons-voss-transponder-decision-illustration.png" alt="definition-illustration" class="simons-voss-transponder-decision-illustration" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="section">
+            <h2 class="section-title">Технологии</h2>
+            <ul class="technology-list content-wrapper">
+                <li class="technology-container">
+                    <div>
+                        <img src="http://192.168.1.112:8080/wp-content/uploads/2023/02/bluetooth.png" alt="bluetooth" class="technology-logo" />
+                    </div>
+                    <p class="technology-name">Bluetooth</p>
+                </li>
+                <li class="technology-container">
+                    <div>
+                        <img src="http://192.168.1.112:8080/wp-content/uploads/2023/02/swift.png" alt="swift" class="technology-logo" />
+                    </div>
+                    <p class="technology-name">Swift</p>
+                </li>
+                <li class="technology-container">
+                    <div>
+                        <img src="http://192.168.1.112:8080/wp-content/uploads/2023/02/Kotlin_mulity_mobile.png" alt="kotlin-mulity-mobile" class="technology-logo" />
+                    </div>
+                    <p class="technology-name">KMM</p>
+                </li>
+                <li class="technology-container">
+                    <div>
+                        <img src="http://192.168.1.112:8080/wp-content/uploads/2023/02/kotlin.png" alt="kotlin" class="technology-logo" />
+                    </div>
+                    <p class="technology-name">Kotlin</p>
+                </li>
+                <li class="technology-container">
+                    <div>
+                        <img src="http://192.168.1.112:8080/wp-content/uploads/2023/02/ktor.png" alt="ktor" class="technology-logo" />
+                    </div>
+                    <p class="technology-name">Ktor</p>
+                </li>
+                <li class="technology-container">
+                    <div>
+                        <img src="http://192.168.1.112:8080/wp-content/uploads/2023/02/c.png" alt="c++" class="technology-logo" />
+                    </div>
+                    <p class="technology-name">C++</p>
+                </li>
+                <li class="technology-container">
+                    <div>
+                        <img src="http://192.168.1.112:8080/wp-content/uploads/2023/02/ci.png" alt="c" class="technology-logo" />
+                    </div>
+                    <p class="technology-name">C</p>
+                </li>
+                <li class="technology-container">
+                    <div>
+                        <img src="http://192.168.1.112:8080/wp-content/uploads/2023/02/objective_c.png" alt="objective_c" class="technology-logo" />
+                    </div>
+                    <p class="technology-name">Objective-C</p>
+                </li>
+            </ul>
+        </section>
+
+        <section class="section staff-comment-section">
+            <div class="comment-container simons-voss-transponder-comment-container">
+                <p class="paragraph">Текущий проект содержит множество технологий, которые связывают отдельные компоненты системы управления умными замками в единое целое - мобильное приложение. Такое решение позволяет легко открывать и управлять замками имея под рукой только смартфон, который в современном мире есть к каждого.</p>
+                <div class="staff-container">
+                    <p class="staff-name">Георгий Старков</p>
+                    <p class="staff-post">IOS-разработчик</p>
+                </div>
+            </div>
+            <div>
+                <img src="http://192.168.1.112:8080/wp-content/uploads/2023/02/georgiy-starkov.png" alt="georgiy-starkov" class="staff-photo" />
+            </div>
+        </section>
+    </div>
+
+    <div class="footer">
+        <div class="page-wrapper">
+            <h3 class="footer-title">Похожие кейсы</h3>
+            <div>
+                <OtherProjectsComponent ids="75" />
+            </div>
+        </div>
+    </div>
+`;
